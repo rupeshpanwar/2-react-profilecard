47e2fad37ce18a5da1b4a71a48627ed088b9589b
Jon Doe <jondoe@doe.com>
2023-02-12 11:35:05 +0800

    step1-basic profilecard setup

---
+++ src/ProfileCard.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+
+const ProfileCard = (props) => {
+ // const { name, title, profileImage, email } = props;
+
+  return (
+    <h1>Profile Card</h1>
+    // <div className="profile-card">
+    //   {/* <img src={profileImage} alt="Profile" className="profile-image"/>
+    //   <h2 className="name">{name}</h2>
+    //   <h3 className="title">{title}</h3>
+    //   <p className="email">{email}</p> */}
+    // </div>
+  );
+};
+
+export default ProfileCard;
